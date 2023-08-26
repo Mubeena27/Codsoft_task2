15d79bbca419d9d03d0d913e9c50ade7af659d48
Mubeena27 <132247772+Mubeena27@users.noreply.github.com>
2023-08-26 14:29:20 +0530

    Add files via upload

---
+++ long_responses.py
@@ -0,0 +1,7 @@
+import random
+R_EATING="I dont like eating anything because Im a bot obviously!"
+def unknown():
+    response=['Could you please re-phrase that?',
+              "....",
+              "Sounds about right",
+              "What does that mean?"][random.randrange(4)]
